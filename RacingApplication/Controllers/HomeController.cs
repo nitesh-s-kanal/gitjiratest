abba61a73a3c307071af99a2dd943f019d8c87d5
nitesh-s-kanal <58078520+nitesh-s-kanal@users.noreply.github.com>
2020-04-12 20:58:40 +0530

    KEY-1 - test intergration 1

--- RacingApplication/Controllers/HomeController.cs
+++ RacingApplication/Controllers/HomeController.cs
@@ -13,7 +13,7 @@ namespace RacingApplication.Controllers
             RacingContext raceContext = new RacingContext();
 
             //Retrive from DB
-            //new changes
+            //new changes test 1
             List<RacingSummary> racingDetails = (from driver in raceContext.Driver
                                                  join racing in raceContext.Racing
                                                  on driver.DriverId equals racing.DriverId
